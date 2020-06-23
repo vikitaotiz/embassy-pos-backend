93f181022131417af0b11917d3d9184bd1b4818c
vikitaotiz <victorotieno598@gmail.com>
2020-06-23 17:42:58 +0300

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    POS Backend
+                    Embassy POS Backend
                 </div>
 
                 <div class="links">
