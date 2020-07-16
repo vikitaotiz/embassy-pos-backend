22714ee9c2c44de4aeb44eda50e888778fbd59e0
vikitaotiz <victorotieno598@gmail.com>
2020-07-16 16:59:50 +0300

    New Updates

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -137,6 +137,17 @@ class SaleController extends Controller
         return SaleResource::collection($sales);
     }
 
+    public function creditSalesIn24hrs(){
+        $from = Carbon::now()->startOfDay()->toDateTimeString();
+        $to = Carbon::now()->endOfDay()->toDateTimeString();
+
+        $sales = Sale::whereBetween('created_at', [$from, $to])
+                    ->where('payment_mode', '=', 'credit')
+                    ->get();
+
+        return SaleResource::collection($sales);
+    }
+
     public function salesReport(Request $request){
 
         if($request->payment_mode){
--- routes/api.php
+++ routes/api.php
@@ -55,6 +55,7 @@ Route::apiResource('/sales', 'SaleController')->middleware('auth:api');
 Route::get('/cashSalesIn24hrs', 'SaleController@cashSalesIn24hrs')->middleware('auth:api');
 Route::get('/mpesaSalesIn24hrs', 'SaleController@mpesaSalesIn24hrs')->middleware('auth:api');
 Route::get('/cardSalesIn24hrs', 'SaleController@cardSalesIn24hrs')->middleware('auth:api');
+Route::get('/creditSalesIn24hrs', 'SaleController@creditSalesIn24hrs')->middleware('auth:api');
 Route::post('/sales_report', 'SaleController@salesReport')->middleware('auth:api');
 Route::post('/sales_report_all', 'SaleController@salesReportAll')->middleware('auth:api');
 Route::get('/sales_last_seven_days', 'SaleController@salesLastSevenDays')->middleware('auth:api');
